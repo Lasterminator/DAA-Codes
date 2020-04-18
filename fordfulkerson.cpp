5462607e4aa55ee13b06eb743e9064a8b40368ac
Subhash Reddy <subhash8111999@gmail.com>
2020-04-18 14:39:43 +0530

    Update fordfulkerson.cpp

--- fordfulkerson.cpp
+++ fordfulkerson.cpp
@@ -17,24 +17,47 @@ void mincut_finder(int residual[V][V], int source, bool visited[]);    //Finding
 void print_mincut(int graph[V][V], bool visited[], int source, int sink);  //Printing mincut edges
 			    
 int main(){
-
-    freopen(OUTPUT, "w", stdout);
-    ifstream cin(INPUT, ifstream::in);
-    
-    int graph[V][V] = {0};
-    
-    int source, sink, vertex1, vertex2, weight;
-    if (cin.is_open()){
-        cin >> source >> sink;
-        //first line has source and sink of graph
-        while(!cin.eof()){
-            cin >> vertex1; cin >> vertex2; cin >> weight;
-            graph[vertex1][vertex2] = weight;
+    int method;
+    cout << "enter 1 for maxflow and 2 for max bipartite problem" << endl;
+    cin >> method;
+    if(method = 1){
+        freopen(OUTPUT, "w", stdout);
+        ifstream cin(INPUT, ifstream::in);
+        
+        int graph[V][V] = {0};
+        
+        int source, sink, vertex1, vertex2, weight;
+        if (cin.is_open()){
+            cin >> source >> sink;
+            //first line has source and sink of graph
+            while(!cin.eof()){
+                cin >> vertex1; cin >> vertex2; cin >> weight;
+                graph[vertex1][vertex2] = weight;
+            }
+            // inserting weights to in graph matrix
         }
-        // inserting weights to in graph matrix
-    }
 
-    max_flow(graph, source, sink);
+        max_flow(graph, source, sink);
+    }
+    else(method = 2){
+        freopen(OUTPUT, "w", stdout);
+        ifstream cin(INPUT, ifstream::in);
+        
+        int graph[V][V] = {0};
+        
+        int source, sink, vertex1, vertex2, weight;
+        if (cin.is_open()){
+            cin >> source >> sink;
+            //first line has source and sink of graph
+            while(!cin.eof()){
+                cin >> vertex1; cin >> vertex2; cin >> weight;
+                graph[vertex1][vertex2] = weight;
+            }
+            // inserting weights to in graph matrix
+        }
+
+        max_flow(graph, source, sink);
+    }
     return 0;
 }
 
